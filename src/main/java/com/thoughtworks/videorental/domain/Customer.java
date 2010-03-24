8292b06746cf14a913587f78cfc832765b484044
Chris Leishman <chris@leishman.org>
2010-03-24 23:07:58 +0000

    Extract & Move Refactorings for requestRenterPoints

--- src/main/java/com/thoughtworks/videorental/domain/Customer.java
+++ src/main/java/com/thoughtworks/videorental/domain/Customer.java
@@ -27,13 +27,7 @@ public class Customer {
 		while (rentals.hasNext()) {
 			Rental each = rentals.next();
 			double thisAmount = each.getCharge();
-
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += each.getFrequentRenterPoints();
 
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
--- src/main/java/com/thoughtworks/videorental/domain/Rental.java
+++ src/main/java/com/thoughtworks/videorental/domain/Rental.java
@@ -39,4 +39,13 @@ public class Rental {
 		}
 		return result;
 	}
+
+	public int getFrequentRenterPoints() {
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
