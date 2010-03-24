bf97ef6eed260e84c9a1dc909674c85d28cdf379
Chris Leishman <chris@leishman.org>
2010-03-24 13:40:15 +0000

    Rename refactorings

--- src/main/java/com/thoughtworks/videorental/domain/Customer.java
+++ src/main/java/com/thoughtworks/videorental/domain/Customer.java
@@ -48,27 +48,27 @@ public class Customer {
 		return result;
 	}
 
-	private double amountFor(Rental each) {
-		double thisAmount = 0;
+	private double amountFor(Rental rental) {
+		double result = 0;
 
 		// determine amounts for each line
-		switch (each.getMovie().getPriceCode()) {
+		switch (rental.getMovie().getPriceCode()) {
 		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			result += 2;
+			if (rental.getDaysRented() > 2)
+				result += (rental.getDaysRented() - 2) * 1.5;
 			break;
 		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
+			result += rental.getDaysRented() * 3;
 			break;
 		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			result += 1.5;
+			if (rental.getDaysRented() > 3)
+				result += (rental.getDaysRented() - 3) * 1.5;
 			break;
 
 		}
-		return thisAmount;
+		return result;
 	}
 
 }
