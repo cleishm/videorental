1545f4181d845083e61a41dd8a5167d6f8037755
Chris Leishman <chris@leishman.org>
2010-03-25 12:42:58 +0000

    MoveMethod getCharge to Movie

--- src/main/java/com/thoughtworks/videorental/domain/Movie.java
+++ src/main/java/com/thoughtworks/videorental/domain/Movie.java
@@ -27,4 +27,27 @@ public class Movie {
 	public String getTitle() {
 		return title;
 	}
+
+	public double getCharge(final int daysRented) {
+		double result = 0;
+	
+		// determine amounts for each line
+		switch (getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2)
+				result += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (daysRented > 3)
+				result += (daysRented - 3) * 1.5;
+			break;
+	
+		}
+		return result;
+	}
 }
--- src/main/java/com/thoughtworks/videorental/domain/Rental.java
+++ src/main/java/com/thoughtworks/videorental/domain/Rental.java
@@ -18,26 +18,7 @@ public class Rental {
 	}
 
 	public double getCharge() {
-		double result = 0;
-	
-		// determine amounts for each line
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-	
-		}
-		return result;
+		return getMovie().getCharge(getDaysRented());
 	}
 
 	public int getFrequentRenterPoints() {
