512cf689ccf77b598a0f3150f6828dbb9658a280
Chris Leishman <chris@leishman.org>
2010-03-25 10:14:44 +0000

    Use Java5 iterator

--- src/main/java/com/thoughtworks/videorental/domain/Customer.java
+++ src/main/java/com/thoughtworks/videorental/domain/Customer.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.videorental.domain;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Customer {
 	private String name;
@@ -22,19 +21,18 @@ public class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Iterator<Rental> rentals = rentalList.iterator();
 		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasNext()) {
-			Rental each = rentals.next();
-			double thisAmount = each.getCharge();
-			frequentRenterPoints += each.getFrequentRenterPoints();
+
+		for (Rental rental : rentalList) {
+			double thisAmount = rental.getCharge();
+			frequentRenterPoints += rental.getFrequentRenterPoints();
 
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
+			result += "\t" + rental.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
-
 		}
+
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
