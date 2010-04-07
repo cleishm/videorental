b1214ee72b7c9c8ca51efafb1a711c047e211d5b
Chris Leishman <chris@leishman.org>
2010-04-07 14:15:07 +0100

    Implemented story #1: "As the marketing co-ordinator, I want to introduce a new pricing model for new release movies that gives 1 free day for a full week of rental so that I can encourage longer rentals of new movies"

--- src/unit/java/com/thoughtworks/videorental/domain/NewReleasePriceTest.java
+++ src/unit/java/com/thoughtworks/videorental/domain/NewReleasePriceTest.java
@@ -8,9 +8,16 @@ public class NewReleasePriceTest {
 	private static final Price NEW_RELEASE_PRICE = new NewReleasePrice();
 
 	@Test
-    public void shouldCalculateCorrectChargeForNewReleaseMovie() throws Exception {
+    public void shouldCalculateCorrectChargeForNewReleaseMovieRentedLessThanAWeek() throws Exception {
     	assertEquals(3.0, NEW_RELEASE_PRICE.getCharge(1));
     	assertEquals(6.0, NEW_RELEASE_PRICE.getCharge(2));
     	assertEquals(9.0, NEW_RELEASE_PRICE.getCharge(3));
     }
+	
+	@Test
+	public void shouldGiveDiscountOfOneDayPerWeekRented() throws Exception {
+		assertEquals(18.0, NEW_RELEASE_PRICE.getCharge(7));
+		assertEquals(36.0, NEW_RELEASE_PRICE.getCharge(14));
+		assertEquals(54.0, NEW_RELEASE_PRICE.getCharge(21));
+	}
 }
