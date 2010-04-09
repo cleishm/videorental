66eaaf714f9a26417b5dbd41db55e0f76b7fc6a3
Chris Leishman <chris@leishman.org>
2010-04-09 11:29:56 +0100

    Only show current rentals

--- src/main/java/com/thoughtworks/videorental/action/ViewRentedMoviesAction.java
+++ src/main/java/com/thoughtworks/videorental/action/ViewRentedMoviesAction.java
@@ -3,13 +3,16 @@ package com.thoughtworks.videorental.action;
 import java.util.Collection;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.thoughtworks.ddd.specification.AndSpecification;
 import com.thoughtworks.videorental.domain.Customer;
 import com.thoughtworks.videorental.domain.Rental;
 import com.thoughtworks.videorental.domain.repository.RentalRepository;
+import com.thoughtworks.videorental.domain.specification.CurrentRentalSpecification;
 import com.thoughtworks.videorental.domain.specification.RentalForCustomerSpecification;
 import com.thoughtworks.videorental.interceptor.CustomerAware;
 
-public class ViewRentedMoviesAction extends ActionSupport implements CustomerAware {
+public class ViewRentedMoviesAction extends ActionSupport implements
+		CustomerAware {
 
 	private final RentalRepository rentalRepository;
 	private Collection<Rental> rentals;
@@ -28,9 +31,12 @@ public class ViewRentedMoviesAction extends ActionSupport implements CustomerAwa
 		return rentals;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public String execute() throws Exception {
-		rentals = rentalRepository.selectSatisfying(new RentalForCustomerSpecification(customer));
+		rentals = rentalRepository.selectSatisfying(new AndSpecification<Rental>(
+						new RentalForCustomerSpecification(customer),
+						new CurrentRentalSpecification()));
 		return SUCCESS;
 	}
 
